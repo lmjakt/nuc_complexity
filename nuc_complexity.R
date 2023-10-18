42b0e50cdcc71f645c030255cab0c8b9326afa92
Martin Jakt <lmjakt@gmail.com>
2023-10-18 09:15:01 +0200

    Entropy plot and fft encode function added
    Added a function to plot entropy across sequences and a function
    that encodes a sequence into a numeric matrix where each column
    represents the presence of absence of a nucleotide.

--- nuc_complexity.R
+++ nuc_complexity.R
@@ -19,3 +19,31 @@ kmer.complexity <- function(seq, k, window.size, olap, comp.beg=FALSE){
     entropy
 }
 
+plot.entropy <- function(ent, xlab="position", ylab="entropy", main="", cols=1:nrow(ent$kws), comp.w=TRUE){
+    l <- length(ent)
+    ent.range <- range( sapply(ent[3:l-2], range) )
+    x <- 1:length(ent[[1]])
+    if(!ent$olap)
+        x <- x * ent$kws[1,'k']
+    plot(1,1, xlim=range(x), ylim=ent.range, xlab=xlab, ylab=ylab, main=main, type='n')
+    for(i in 1:nrow(ent$kws)){
+        x <- 1:length(ent[[i]])
+        if(!ent$olap)
+            x <- x * ent$kws[i,'k']
+        beg <- ifelse(comp.w, ent$kws[i,'ws'], 1)
+        r <- beg:length(x)
+        lines(x[r], ent[[i]][r], col=cols[i])
+    }
+}
+
+## The following is in order to obtain an encoding that can be used
+## with fft() to obtain the power spectrum for each nucleotide
+fft.encode <- function(seq, center=TRUE){
+    toN <- function(char){
+        as.numeric( toupper(utf8ToInt(seq)) == utf8ToInt(char) )
+    }
+    m <- sapply(c(A="A", C="C", G="G", T="T"), toN)
+    if(center)
+        m <- t( t(m) - colMeans(m) )
+    m
+}
